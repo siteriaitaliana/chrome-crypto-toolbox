6b74c0985f09dbbd2dc6eb3498a71e85e607393c
Lorenzo Urbini <lorenzo.urbini@consensys.net>
2022-08-08 10:41:40 +0200

    bug fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 document.getElementById('decimalInput').addEventListener("focus", function(event) {
   document.getElementById('decimalInput').value = ''
+  document.getElementById('hexaInput').value = ''
 })
 document.getElementById('hexaInput').addEventListener("focus", function(event) {
   document.getElementById('hexaInput').value = ''
+  document.getElementById('decimalInput').value = ''
 })
 document.getElementById('etherScanInput').addEventListener("focus", function(event) {
   document.getElementById('etherScanInput').value = ''
@@ -46,23 +48,21 @@ document.getElementById('search').addEventListener("click", function(event) {
 });
 
 document.getElementById('click_me').addEventListener("click", function(event) {
-  (function(event) {
-    let decimalVal = document.getElementById('decimalInput').value
-    let hexaVal = document.getElementById('hexaInput').value
-    if (decimalVal && hexaVal){
-        document.getElementById('output').innerHTML = "Input the decimal or the hex not both together"
-    } else if(decimalVal) {
-      // https://web3js.readthedocs.io/en/v1.2.6/web3-utils.html#tohex
-      // Will auto convert any given value to HEX. Number strings will interpreted as numbers. Text strings will be interpreted as UTF-8 strings.
-        document.getElementById('output').innerHTML = Web3.utils.toHex(decimalVal)
-    } else if (hexaVal){
-      if(Web3.utils.isHexStrict(hexaVal)){
-        document.getElementById('output').innerHTML = Web3.utils.hexToNumberString(hexaVal)
-      } else {
-        document.getElementById('output').innerHTML = "Check the HEX is prefixed with 0x and a valid HEX"
-      }
-    } else if (!decimalVal && !hexaVal){
-      document.getElementById('output').innerHTML = "Input either the decimal or the hex"
+  let decimalVal = document.getElementById('decimalInput').value
+  let hexaVal = document.getElementById('hexaInput').value
+  if (decimalVal && hexaVal){
+      document.getElementById('output').innerHTML = "Input the decimal or the hex not both together"
+  } else if(decimalVal) {
+    // https://web3js.readthedocs.io/en/v1.2.6/web3-utils.html#tohex
+    // Will auto convert any given value to HEX. Number strings will interpreted as numbers. Text strings will be interpreted as UTF-8 strings.
+      document.getElementById('output').innerHTML = Web3.utils.toHex(decimalVal)
+  } else if (hexaVal){
+    if(Web3.utils.isHexStrict(hexaVal)){
+      document.getElementById('output').innerHTML = Web3.utils.hexToNumberString(hexaVal)
+    } else {
+      document.getElementById('output').innerHTML = "Check the HEX is prefixed with 0x and a valid HEX"
     }
-  }).call(document.getElementById('click_me'), event);
+  } else if (!decimalVal && !hexaVal){
+    document.getElementById('output').innerHTML = "Input either the decimal or the hex"
+  }
 });
